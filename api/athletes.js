e4555b8371d06bb624afda45e39a594610a4e375
Angel Mgl <angelemegeele@gmail.com>
2020-12-13 14:14:33 -0300

    Método GET de sports y athletes

--- api/athletes.js
+++ api/athletes.js
@@ -3,9 +3,26 @@ const router = express.Router();
 
 const Athlete = require('../models/athlete');
 
-router.get('/', (req, res) => {
-    res.send('obteniendo recurso de atletas...');
+router.get('/', async (req, res, next) => {
+    try {
+        const athletes = await Athlete.find().populate('sports');
+        res.status(200).json(athletes);
+    } catch (error) {
+        res.status(400);
+        next(error)
+    }
 });
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const athlete = await Athlete.findById(id).populate('sports');
+        res.status(200).json(athlete);
+    } catch (error) {
+        res.status(400);
+        next(error);
+    }
+})
 
 router.post('/', async (req, res, next) => {
     const athlete = new Athlete(req.body)
--- api/sports.js
+++ api/sports.js
@@ -1,11 +1,29 @@
 const express = require('express');
+const { findById } = require('../models/sport');
 const router = express.Router();
 
 const Sport = require('../models/sport')
 
-router.get('/', (req, res) => {
-    res.send('obteniendo recurso de deportes...');
+router.get('/', async (req, res, next) => {
+    try {
+        const sports = await Sport.find();
+        res.status(200).json(sports);
+    } catch (error) {
+        res.status(400);
+        next(error);
+    }
 });
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const sport = await Sport.findById(id);
+        res.status(200).json(sport);
+    } catch (error) {
+        res.status(400);
+        next(error);
+    }
+})
 
 router.post('/', async (req, res, next) => {
     const sport = new Sport(req.body);
